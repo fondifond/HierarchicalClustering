fe6ed09d6e21b3b1497e059e3cc8d8cb6694334e
fondifond <fondifond.noo@gmail.com>
2015-07-08 08:43:59 +0300

    add templating

--- demo.php
+++ demo.php
@@ -6,7 +6,6 @@ use \HierarchicalClustering\Clustering;
 use \HierarchicalClustering\Links\AverageLink;
 use \HierarchicalClustering\Distances\EuclideanDistance;
 
-
 $input = [
     [1,2,3],
     [2,2,2],
@@ -18,10 +17,12 @@ $input = [
     [2,5,2],
     [4,2,5],
 ];
+
 $object = new Clustering(
     $input,
     new EuclideanDistance(),
     new AverageLink()
 );
 $clusters = $object->getCluster();
+
 var_dump($clusters);
